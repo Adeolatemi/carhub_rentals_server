8ccb545612d203f037b6c68cb234d43015b13032
Adeola <adeolafatosin@gmail.com>
2026-04-15 22:24:16 +0100

    Add company field to User model for partners

--- prisma/migrations/20260323111457_add_kyc_fields_to_user/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "kycFile" TEXT;
-ALTER TABLE "User" ADD COLUMN "kycStatus" TEXT;
--- prisma/migrations/20260323111845_add_kyc_history/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "KycHistory" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "userId" TEXT NOT NULL,
-    "filename" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "KycHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
